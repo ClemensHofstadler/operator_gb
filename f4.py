d52165f653f19e5a77cc54ba3007ac4a2df1bea9
ClemensHofstadler <40797495+ClemensHofstadler@users.noreply.github.com>
2024-02-08 16:42:36 +0100

    Bugfix for ideals containing 1

---
+++ f4.py
@@ -0,0 +1,320 @@
+# coding: utf-8
+"""
+F4
+================
+
+Module that implements the F4 algorithm in the free algebra that
+allows to trace cofactors.
+
+AUTHOR:
+
+- Clemens Hofstadler (2023-02-09)
+
+"""
+
+#############################################################################
+#  Copyright (C) 2020 Clemens Hofstadler (clemens.hofstadler@jku.at).       #
+#                                                                           #
+#  Distributed under the terms of the GNU General Public License (GPL)      #
+#  either version 2, or (at your option) any later version                  #
+#                                                                           #
+#  http://www.gnu.org/licenses/                                             #
+#############################################################################
+
+from time import time
+from copy import copy
+
+import ahocorasick
+
+from .ambiguity import *
+from .cofactor import Cofactor
+from .free_algebra import MyFreeAlgebra
+from .nc_monomial import NCMonomial
+from .nc_polynomial import NCPolynomial
+from .normal_form import interreduce
+from .crit_pair import CritPair
+from .linear_algebra import faugere_lachartre
+
+class F4():
+
+    def __init__(self,I):
+        self.__parent = I.parent()
+        self.__gens = I.gens()
+        self.__internal_gens = I.internal_gens()
+        self.__criterion = True
+        self.__maxdeg = -1
+        self.__lm = ahocorasick.Automaton()
+        self.__suffix_trie = ahocorasick.Automaton()
+        self.__verbose = 0
+        self.__amb = []
+        self.__G = []
+        self.__constant_flag = False
+############################################################################        
+    def lm(self): return list(self.__lm.items()) 
+    def parent(self): return self.__parent
+    def gens(self): return self.__gens
+    def internal_gens(self): return self.__internal_gens
+    def amb(self): return self.__amb
+    def G(self): return self.__G
+############################################################################
+    def clear(self):
+        self.__lm.clear()
+        self.__suffix_trie.clear()
+        self.__amb = []
+        self.__G = []
+        self.__constant_flag = False
+############################################################################
+# Compute Gröbner basis
+############################################################################
+    def compute_basis(self,maxiter=10,maxdeg=-1,trace_cofactors=True,criterion=True,reset=True,verbose=0):
+        global nr_pairs
+        global zero_reductions
+        
+        self.__maxdeg = maxdeg
+        self.__criterion = criterion
+        self.__verbose = verbose
+        
+        nr_pairs = 0
+        zero_reductions = 0
+        
+        if reset or not self.__G: 
+            self.prepare_input(verbose,trace_cofactors=trace_cofactors)
+            # constant_flag checks if GB contains 1
+            if self.__constant_flag: return self.__G
+            self.compute_ambiguities()
+        
+        self.__already_rewritten = len(self.__G)
+        oldlen = len(self.__G)      
+        count = 0
+                       
+        while count < maxiter and self.__amb:
+            count += 1
+            
+            start = time() 
+            PP = self.reduction(trace_cofactors=trace_cofactors)
+            if verbose > 0:
+                print("Reduction took: %.5f" % (time()-start))
+                            
+            self.add_polynomials(PP)  
+            if self.__constant_flag: break                                             
+            self.compute_ambiguities(oldlen)
+            oldlen = len(self.__G)
+            if verbose > 0:
+                print("Iteration %d finished. G has now %d elements.\n" % (count,len(self.__G)))
+                    
+        if (not self.__amb or self.__constant_flag) and verbose > 0:
+            print("All critial pairs could be reduced to zero.")
+            
+        if verbose > 1:
+            print("In total, %d critical pairs were reduced." % nr_pairs)
+            print("In total, %d reductions to zero occurred." % zero_reductions)
+        
+        if trace_cofactors:
+            self.rewrite_cofactors()
+            
+        return self.__G
+
+############################################################################
+    def compute_ambiguities(self,oldlen=0):
+        
+        G = self.__G
+        maxdeg = self.__maxdeg
+        verbose = self.__verbose
+        criterion = self.__criterion
+                
+        words = [str(g.lm()) for i,g in enumerate(G)]         
+        l = len(self.__amb)
+            
+        start = time()
+        prefix_trie = self.__lm
+        suffix_trie = self.__suffix_trie
+        for i in range(oldlen,len(words)):
+            amb_i = Ambiguity.generate_with_tries(prefix_trie,suffix_trie,words,i,self.__amb,criterion=criterion)
+            if maxdeg > 0: amb_i = [a for a in amb_i if a.degree() <= maxdeg]
+            if criterion:
+                self.__amb = Ambiguity.chain_criterion(self.__amb,amb_i,words[i],i)
+            self.__amb += amb_i
+                                    
+        if verbose > 0:
+            print("%d ambiguities in total (computation took %.5f)" % (len(self.__amb), time()-start))
+                             
+        self.__amb.sort(key = lambda p : p.degree())
+############################################################################
+# Reduction & Symbolic Preprocessing
+############################################################################
+    def reduction(self,trace_cofactors=True):
+        global nr_pairs
+        global zero_reductions
+                
+        G = self.__G
+        amb = self.__amb
+        verbose = self.__verbose
+        
+        # choose ambiguities            
+        d = amb[0].degree()
+        P = [a for a in amb if a.degree() == d]
+        self.__amb = amb[len(P):]
+        P = [CritPair(a,G[a.i()],G[a.j()]) for a in P]        
+        # remove zero S-polynomials
+        P = [pair for pair in P if pair.degree() > -1]
+        if not P: return []
+        
+        if verbose > 0:
+            print("%d critical pairs will be reduced." % len(P))
+        
+        nr_pairs += len(P) 
+        
+        # do symbolic preprocessing
+        F = flatten([pair.fg() for pair in P])
+        pivot_rows,pivot_columns,columns = self.symbolic_preprocessing(F)
+
+        #split critical pairs into pivot and non-pivot rows
+        rest_rows = [pair.g() for pair in P]
+        for pair in P:
+            f = pair.f()
+            if f.lm() in pivot_columns:
+                rest_rows.append(f)
+            else:
+                pivot_columns.add(f.lm())
+                pivot_rows.append(f)
+        
+        
+        #seperate pivot and non-pivot columns
+        rest_columns = list(columns - pivot_columns)
+        pivot_columns = list(pivot_columns)
+        pivot_columns.sort(reverse=True)
+        rest_columns.sort(reverse=True)
+        
+        size_A = len(pivot_rows)
+
+        columns = {m:i for (i,m) in enumerate(pivot_columns + rest_columns)}
+        pivot_rows.sort(key=lambda f: f.lm(),reverse=True)
+        rows = pivot_rows + rest_rows
+        
+        # M...the rref we are interested in
+        # T,T1...the transformation matrix
+        T,T1,M = faugere_lachartre(rows,columns,size_A,trace_cofactors=trace_cofactors)
+                 
+        PP = self.matrix_to_polies(T,T1,M,rest_columns,rows,size_A)
+                
+        zero_reductions += len(P) - len(PP)
+            
+        return PP
+############################################################################
+    def symbolic_preprocessing(self,F):
+        done = {f.lm() for f in F}
+        todo = {m for f in F for m in f.monomials()} - done
+        reducers = []
+        pivot_columns = set()
+        while todo:
+            m = todo.pop()
+            done.add(m)
+            agb = self.find_reducer(str(m))
+            if agb:
+                assert m == agb.lm()
+                reducers.append(agb)
+                pivot_columns.add(agb.lm())
+                todo.update(set(agb.monomials()).difference(done))
+        return reducers,pivot_columns,done
+############################################################################
+    def find_reducer(self,m):
+        lm = self.__lm
+        G = self.__G
+                
+        reducer = list(lm.iter(m))
+        #reducer = next(lm.iter(m),None)
+        if not reducer: return None
+        #k, (i,lm) = reducer 
+        k,(i,lm) = min(reducer, key=lambda r:r[1][0])
+        g = G[i]
+        a = m[:k-len(lm)+1]
+        b = m[k+1:]
+        agb = g.lrmul(a,b)
+        agb.append_cofactor(Cofactor(1,a,i,b,self.__parent))
+        return agb
+############################################################################        
+    def matrix_to_polies(self,T,T1,M,rest_columns,rows,n):
+    
+        #compute polynomials
+        pos = M.nonzero_positions()
+        if not pos: return []
+        rank = pos[-1][0]+1
+        coefficients = [[] for i in range(rank)]
+        monomials = [[] for i in range(rank)]
+        for (i,j),c in reversed(list(M.dict().items())):
+            coefficients[i].append(c)
+            monomials[i].append(rest_columns[j])
+    
+        PP = [NCPolynomial(c,m) for c,m in zip(coefficients,monomials)]
+            
+        # take care of cofactors
+        for (i,j),tij in T1.dict().items():
+            if i >= rank: continue
+            PP[i].append_cofactors([c * tij for c in rows[j].cofactors()])
+
+        for (i,j),tij in T.dict().items():
+            if i >= rank: continue
+            PP[i].append_cofactors([c * tij for c in rows[j+n].cofactors()])
+
+        PP.sort(key=lambda f : f.lm())
+        return PP
+############################################################################        
+# Auxiliary
+############################################################################      
+    def prepare_input(self,verbose,trace_cofactors=True):
+        
+        self.clear()
+        # change data structure
+        A = self.__parent
+        G = [copy(f) for f in self.__internal_gens]
+        
+        # make monic and add cofactors
+        for i,g in enumerate(G):
+            c = g.make_monic()
+            g.reset_cofactors()
+            if trace_cofactors: g.append_cofactor(Cofactor(1/c,'',i,'',A))
+        
+        # interreduce generators
+        G = interreduce(G)
+        if verbose > 0:
+            print("Interreduced the generators from %d elements to %d elements.\n" % (len(self.__gens),len(G)))
+        
+        # add interreduced generators to GB    
+        self.add_polynomials(G)
+        
+############################################################################                    
+    def add_polynomials(self,PP):
+        oldlen = len(self.__G)
+        for i,p in enumerate(PP):
+            m = str(p.lm())
+            if m:
+                self.__lm.add_word(m,(i+oldlen,m))
+                self.__suffix_trie.add_word(m[::-1],(oldlen+i,m))   
+            else:
+                self.__constant_flag = True
+                break
+
+        self.__lm.make_automaton()
+        self.__G += PP
+############################################################################                
+    def rewrite_cofactors(self):
+        
+        A = self.__parent
+        G = self.__G
+                
+        for g in G[self.__already_rewritten:]:
+            cofactors_tmp = []
+            for cofactor in g.cofactors():
+                c,a,i,b = cofactor.caib()
+                cofactors_tmp += [t.lrmul(a,b) * c for t in G[i].cofactors()]
+            g.reset_cofactors()
+            g.append_cofactors(cofactors_tmp)
+            g.simplify_cofactors()
+
+
+
+
+
+
+
+
---
+++ nc_polynomial.py
@@ -0,0 +1,269 @@
+# coding: utf-8
+"""
+NCPoly
+================
+
+A class that provides basic funcionality for noncommutative polynomials
+
+AUTHOR:
+
+- Clemens Hofstadler (2023-02-09)
+
+"""
+
+from sage.all import prod
+from copy import copy,deepcopy
+
+import operator_gb.free_algebra
+from .nc_monomial import NCMonomial
+from .cofactor import Cofactor
+
+############################################################################
+############################################################################
+# NCPolynomial
+############################################################################
+############################################################################
+class NCPolynomial:
+
+    def __init__(self,coeffs,mons=None):
+        if mons is not None:
+            self.__lm = mons[-1]
+            self.__mons = mons
+            self.__coeffs = coeffs  
+            self.__cofactors = []
+        else:
+            P = coeffs.parent()
+            A = operator_gb.free_algebra.MyFreeAlgebra(P.base_ring(), P.gens())
+            f = A(coeffs)
+            self.__lm = f.__lm
+            self.__mons = f.__mons
+            self.__coeffs = f.__coeffs
+            self.__cofactors = []
+###########################################################################
+    def parent(self): return self.__lm.parent()
+    def lm(self): return self.__lm
+    def lc(self): return self.__coeffs[-1]
+    def monomials(self): return self.__mons
+    def coefficients(self): return self.__coeffs
+    def cofactors(self): return self.__cofactors
+############################################################################
+    def variables(self,internal=False):
+        T = self.parent().translator()
+        out = set()
+        for m in self.__mons:
+            out.update(set(str(m)))
+        return [T(x,to_internal=internal) for x in out]
+############################################################################
+    def coefficients_monomials(self): return zip(self.__coeffs, self.__mons)
+############################################################################
+    def __copy__(self): return NCPolynomial(copy(self.__coeffs),deepcopy(self.__mons))
+############################################################################
+    def to_native(self): return self.parent()(self)
+############################################################################
+    def __repr__(self):
+        s = ""
+        for c,m in zip(self.__coeffs,self.__mons):
+            # sign
+            if c == 0: continue
+            if c > 0: s += " + "
+            else: s += " - "
+            
+            # m is not 1
+            if str(m):
+                if abs(c) != 1: 
+                    s += str(abs(c)) + "*"        
+                s += m.pretty_print()
+            # special case m = 1
+            else:
+                s += str(abs(c))
+                
+        # remove first + if existent
+        if not s: s = "0"
+        elif s[1] == "+": s = s[3:]
+        else: s = s[1:] 
+        
+        return s
+###########################################################################
+    def __eq__(self, other):
+        if len(self.__coeffs) != len(other.__coeffs): return False
+        return self.__lm == other.__lm and self.__coeffs == other.__coeffs and self.__mons == other.__mons
+    def __ne__(self,other):
+        return not (self == other)
+############################################################################
+    def  __hash__(self):
+        return hash((tuple(self.__mons),tuple(self.__coeffs)))
+############################################################################
+    def __neg__(self):
+        out = self.__copy__()
+        out.__coeffs = [-c for c in self.__coeffs]
+        return out   
+############################################################################
+    def __add__(self,other):
+        if self.parent() != other.parent():
+            raise ValueError("Elements have to be defined over the same ring.")
+        
+        d = {m:c for c,m in self.coefficients_monomials()}
+        for c,m in other.coefficients_monomials():
+            if m in d: d[m] += c
+            else: d[m] = c
+        
+        coeffs,mons = dict_to_poly(d)
+        if not mons: return NCPolynomial.zero(self.parent())
+        return NCPolynomial(coeffs,mons)
+############################################################################
+    def  __sub__(self,other): return self.__add__(other.__neg__())
+############################################################################    
+    def __mul__(self,other):
+        if self.parent() != other.parent():
+            raise ValueError("Elements have to be defined over the same ring.")
+        
+        d = dict()
+        for c1,m1 in self.coefficients_monomials():
+            for c2,m2 in other.coefficients_monomials():
+                m1m2 = m1*m2
+                if m1m2 in d: d[m1m2] += c1*c2
+                else: d[m1m2] = c1*c2
+        
+        coeffs,mons = dict_to_poly(d)
+        if not mons: return NCPolynomial.zero(self.parent())
+        return NCPolynomial(coeffs,mons)
+###########################################################################
+    def coeff_mul(self,c):
+        out = self.__copy__()
+        out.__coeffs = [coeff * c for coeff in self.__coeffs]
+        return out
+###########################################################################
+    def degree(self): return len(self.__lm)
+###########################################################################
+    @staticmethod
+    def zero(P): return NCPolynomial([P.zero()],[NCMonomial('',P)])
+############################################################################                
+    def subs(self,x,f):
+        T = self.parent().translator()
+        to_replace = T(x)
+        out = self.__copy__()
+        i = 0
+        while i < len(out.__mons):
+            m = out.__mons[i]
+            if to_replace not in str(m):
+                i += 1
+                continue
+            else:
+                out.__mons.remove(m)
+                coeff = out.__coeffs.pop(i)
+                a,b = str(m).split(to_replace,1)
+                out.__mons += [m.lrmul(a,b) for m in f.__mons]
+                out.__coeffs += [coeff * c for c in f.__coeffs]
+        
+        # clean up
+        d = dict()
+        for c,m in out.coefficients_monomials():
+            if m in d: d[m] += c
+            else: d[m] = c
+        
+        coeffs,mons = dict_to_poly(d)
+        if not mons: return NCPolynomial.zero(self.parent())
+        return NCPolynomial(coeffs,mons)        
+############################################################################                
+    def set_to_zero(self):
+        P = self.parent()
+        self.__coeffs = [P.zero()]
+        self.__mons = [NCMonomial('',P)]
+        self.__lm = self.__mons[-1]
+############################################################################
+    def is_zero(self): 
+        z = self.__lm.parent().zero()
+        return self.__coeffs == [z]*len(self.__coeffs)
+############################################################################
+    def change_parent(self,A):
+        if self.is_zero():
+            return NCPolynomial.zero(A)
+        return A(self.__repr__())
+############################################################################    
+    def make_monic(self):
+        """
+        Really update self
+        """
+        lc = self.__coeffs[-1]
+        if lc != 1: self.__coeffs = [c / lc for c in self.__coeffs]
+        return lc
+############################################################################
+    def append_cofactor(self, c):
+        self.__cofactors.append(c)
+############################################################################
+    def reset_cofactors(self):
+        self.__cofactors = []
+############################################################################
+    def append_cofactors(self, c):
+        self.__cofactors += c
+############################################################################
+    def lrmul(self, l, r):
+        mons = [m.lrmul(l,r) for m in self.__mons]
+        coeffs = copy(self.__coeffs)
+        return NCPolynomial(coeffs,mons)
+############################################################################   
+    def is_left_multiple_of(self, g):
+        return self.is_one_sided_multiple_of(g,'left')
+############################################################################   
+    def is_right_multiple_of(self, g):
+        return self.is_one_sided_multiple_of(g,'right')
+############################################################################   
+    def is_one_sided_multiple_of(self, g, side):
+        f = self
+        # special case: at least one argument is zero
+        if f.is_zero(): return True
+        if g.is_zero(): return False
+        
+        assert side == 'left' or side == 'right'
+                
+        A = self.parent()   
+        if not isinstance(g,NCPolynomial): g = A(g)
+        lm_g = str(g.lm())
+        coeffs,mons = [],[]
+        
+        while not f.is_zero():
+            lm_f = str(f.lm())
+            
+            if side == 'left':
+                if not lm_f.endswith(lm_g): return False
+                l = lm_f[:-len(lm_g)]
+                r = ''
+                mons.append(NCMonomial(l,A))
+            else:
+                if not lm_f.startswith(lm_g): return False
+                l = ''
+                r = lm_f[len(lm_g):]
+                mons.append(NCMonomial(r,A))
+            
+            c = f.lc() / g.lc()
+            coeffs.append(c)
+            f = f - g.lrmul(l,r).coeff_mul(c)
+        
+        quo = NCPolynomial(coeffs, mons)
+        return quo    
+############################################################################
+    def normal_cofactors(self,gens):
+        F = self.__lm.parent().F()
+        cofactors = [c.to_free_algebra(F,gens[c.i()]) for c in self.__cofactors]   
+        return cofactors 
+############################################################################
+    def expand_cofactors(self,gens):
+        cofactors = self.normal_cofactors(gens)
+        return sum(map(prod,cofactors))
+############################################################################                
+    def simplify_cofactors(self):
+        d = dict()
+        A = self.parent()
+        for cofactor in self.__cofactors:
+            c,a,i,b = cofactor.caib()
+            if (a,i,b) in d: d[(a,i,b)] += c
+            else: d[(a,i,b)] =  c
+        
+        self.__cofactors = [Cofactor(c,a,i,b,A) for (a,i,b),c in d.items() if c]
+
+def dict_to_poly(d):
+    d = list(d.items())
+    d.sort(key=lambda p : p[0])
+    coeffs = [c for m,c in d if c != 0]
+    mons   = [m for m,c in d if c != 0]
+    return coeffs, mons
